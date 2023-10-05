67eb6f7a22bad614bf5052caef42f79262bb0cd0
Murrrphy <444575716@qq.com>
2023-10-05 20:20:30 +0800

    filter类实现登录拦截

---
+++ src/main/java/com/murrrphy/filter/LoginCheckFilter.java
@@ -0,0 +1,64 @@
+package com.murrrphy.filter;
+
+import com.alibaba.fastjson.JSONObject;
+import com.murrrphy.pojo.Result;
+import com.murrrphy.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //1.获取请求的url
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String url = request.getRequestURL().toString();
+        log.info("请求的url为：{}", url);
+
+        //2.判断是否为登录操作
+        if(url.contains("login")){
+            log.info("登录操作，直接放行");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //3.如果不是登录操作，获取请求头中的token
+        String jwt =request.getHeader("token");
+
+        //4.判断令牌是否存在
+        if(!StringUtils.hasLength(jwt)){
+            log.info("token为空！！");
+
+            Result error = Result.error("NOT_LOGIN");
+            String notLogin = JSONObject.toJSONString(error);
+            response.getWriter().write(notLogin);
+            return;
+        }
+
+        //5.如果存在令牌，判断是否有效
+        try {
+            JwtUtils.parseJwt(jwt);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("令牌无效");
+
+            Result error = Result.error("NOT_LOGIN");
+            String notLogin = JSONObject.toJSONString(error);
+            response.getWriter().write(notLogin);
+            return;
+        }
+
+        //6.登录成功
+        log.info("登录成功！");
+        filterChain.doFilter(request, response);
+    }
+}
